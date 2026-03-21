9442b7e297bbd1dcaccb9db5fe58cdc9b8d62356
keygen12 <asusflexflow@gmail.com>
2026-03-21 10:48:36 -0400

    Удалил файл text

--- file_about_result_of_size.sh
+++ file_about_result_of_size.sh
@@ -1,1 +1,1 @@
-find /home/eugene/ -type f -size +100M -printf  '%20s %p\n' 2>/dev/null | sort -n | awk '{printf "%10.1f MB %s\n", $1/1024/1024, $2}'
+find /home/$USER/ -type f -size +10M -printf  '%20s %p\n' 2>/dev/null | sort -n | awk '{printf "%10.1f MB %s\n", $1/1024/1024, $2}'
